af1d79518c44544a8a8600027819a3d76fb6272c
priyankathomxs <200587559@student.georgianc.on.ca>
2024-06-02 21:30:26 -0400

    added all the project files needed for render from  ICE4

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -26,6 +26,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = __importStar(require("express"));
 const router = express.Router();
 const movie_1 = require("../Controllers/movie");
-router.get('/', function (req, res, next) { (0, movie_1.DisplayMovieList)(req, res, next); });
+router.get('/', (req, res, next) => { (0, movie_1.DisplayMovieList)(req, res, next); });
+router.get('/:id', (req, res, next) => { (0, movie_1.DisplayMovieById)(req, res, next); });
+router.post('/add', (req, res, next) => { (0, movie_1.AddMovie)(req, res, next); });
+router.put('/update/:id', (req, res, next) => { (0, movie_1.UpdateMovie)(req, res, next); });
+router.delete('/delete/:id', (req, res, next) => { (0, movie_1.DeleteMovie)(req, res, next); });
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- Server/Routes/index.ts
+++ Server/Routes/index.ts
@@ -1,10 +1,22 @@
 import * as express from 'express';
 const router = express.Router();
 
-import{ DisplayMovieList } from '../Controllers/movie';
+import{ DisplayMovieList, DisplayMovieById, AddMovie, UpdateMovie, DeleteMovie} from '../Controllers/movie';
 
 /* List of Routes (endpoints) */
 /* GET Movie List. */
-router.get('/', function(req, res, next) { DisplayMovieList(req, res, next);});
+router.get('/', (req, res, next) => { DisplayMovieList(req, res, next);});
+
+/*GET Movie by Id */
+router.get('/:id', (req, res, next) => { DisplayMovieById(req, res, next); });
+
+/* Add Movie */
+router.post('/add', (req, res, next) => { AddMovie(req, res, next); });
+
+/* Update Movie */
+router.put('/update/:id', (req, res, next) => { UpdateMovie(req, res, next); });
+
+/* Delete Movie */
+router.delete('/delete/:id', (req, res, next) => { DeleteMovie(req, res, next); });
 
 export default router;
